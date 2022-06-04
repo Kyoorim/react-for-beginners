706b365d2d0cba36284ccc2fa3856ce77e07e952
Kyoorim <kyoorim88@gmail.com>
2022-06-04 18:46:11 +0900

    react 이론 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+// import Button from "./Button";
+// import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("i run only once/ Call the API");
+  };
+  useEffect(iRunOnlyOnce, []); // []은 지켜볼 대상이 없다는 의미로 딱 한번만 실행된다는 뜻
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]); // keyword가 변화할 때 코드를 실행할 것이라고 react.js에게 알려주는 것
+  useEffect(() => {
+    console.log("i run when 'keyword & counter' changes");
+  }, [keyword, counter]); // 동시에 여러개 적는 것도 가능
   return (
     <div>
-      <h1>Welcome back! </h1>
+      {/* <h1 className={styles.title}>Welcome back! </h1>
+      <Button text={"Continue"} /> */}
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      ></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
---
+++ src/App_cleanUp.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+function Hello() {
+  //   function byFn() {
+  //     console.log("bye");
+  //   }
+  //   function hiFn() {
+  //     console.log("created");
+  //     return byFn;
+  //   }
+  //   useEffect(hiFn, []);
+  //   return <h1>Hello</h1>;
+  useEffect(() => {
+    console.log("hi");
+    return () => console.log("bye");
+  }, []);
+  //   useEffect(function () {
+  //     console.log("hi");
+  //     return function () {
+  //       console.log("bye");
+  //     };
+  //   }, []);
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
+
+export default App_cleanUp;
